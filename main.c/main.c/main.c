ecd1be3812801672b9ab7583e896d75438e3c368
Neha Deshpande <106668510+nehadesh933@users.noreply.github.com>
2022-06-01 16:57:25 +0530

    Add files via upload

---
+++ main.c/main.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  main.c
+//
+//  Created by Neha R Deshpande on 01/06/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
